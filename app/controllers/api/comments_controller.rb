339aece760e0103f18edfad1f76910a680772f21
NaokiShizuka <naokishizuka14@gmail.com>
2016-06-07 00:29:44 +0900

    test2

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,6 +1,7 @@
 class Api::CommentsController < ApplicationController
   protect_from_forgery :except => [:create]
   # test
+  # test2
   def index
     @comments = Comment.all
     render json: @comments
